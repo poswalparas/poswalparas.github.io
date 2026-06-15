03191586f1918bddf8b6f3fa36347599d82c862b
poswalparas <poswalparas@gmail.com>
2026-06-15 20:57:12 +0530

    Update hero-animation.js

--- assets/js/hero-animation.js
+++ assets/js/hero-animation.js
@@ -145,7 +145,7 @@ function createHexagonalLattice(){
     }
 
 }
-
+createHexagonalLattice();
 // ==========================================
 // Animation
 // ==========================================
